6d6e631145c766ff7a81b1f6b69757bb42478032
xujie <xujie@yixia.com>
2021-03-10 17:17:48 +0800

    用户角色

--- src/api/user.ts
+++ src/api/user.ts
@@ -25,3 +25,21 @@ export const getInfo = () => {
     headers: { 'content-type': 'application/x-www-form-urlencoded' }
   })
 }
+
+export const getUserPages = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/user/getUserPages',
+    data
+  })
+}
+
+export const forbidUser = (userId: string | number) => {
+  return request({
+    method: 'POST',
+    url: '/boss/user/forbidUser',
+    params: {
+      userId
+    }
+  })
+}
